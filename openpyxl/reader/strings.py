873cc3dda43578c098843ebe930325c879d20baf
VariantXYZ <variantxyz@gmail.com>
2020-08-06 21:22:25 -0700

    When getting formatted text, apply bold and italic tags if necessary

--- openpyxl/reader/strings.py
+++ openpyxl/reader/strings.py
@@ -14,10 +14,23 @@ def read_string_table(xml_source):
 
     for _, node in iterparse(xml_source):
         if node.tag == STRING_TAG:
-            text = Text.from_tree(node).content
+            original_text = Text.from_tree(node)
+
+            if len(original_text.formatted) == 0:
+                text = original_text.content
+            else:
+                text = ""
+                for x in original_text.formatted:
+                    txt = x.t # Will handle InlineFont and RichText
+                    if x.rPr:
+                        if x.rPr.b == True:
+                            txt = f"<b>{txt}</b>"
+                        if x.rPr.i == True:
+                            txt = f"<i>{txt}</i>"
+                    text += txt
+
             text = text.replace('x005F_', '')
             node.clear()
-
             strings.append(text)
 
     return strings
